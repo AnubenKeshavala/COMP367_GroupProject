fbe503bd9147af6bfa067dad626a9fb3c3158315
Anuben Keshavala <akeshav4@my.centennialcollege.ca>
2022-12-08 12:25:34 -0500

    updated routes - issue solved

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,15 +1,10 @@
-// Load the 'index' controller
-const index = require('../controllers/index.server.controller');
-
-// Define the routes module' method
+const index = require("../controllers/index.server.controller");
 module.exports = function (app) {
-
-    app.get('/', function (req, res) {
-        res.render('index', {
-            info: "see the results in console window"
-        })
+  app.get("/", function (req, res) {
+    res.render("index", {
+      info: "see the results in console window",
     });
-
-    app.get('/run', index.trainAndPredict);
-
+  });
+  app.get("/run", index.trainAndPredict);
+  app.post("/runwithpar", index.trainAndPredictWithParam);
 };
